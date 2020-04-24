d5e23e625bf45f625c55a265f08536bf1bba51a9
yudharakagilang <gilang.yudharaka@ui.ac.id>
2020-04-24 12:08:58 +0700

    chart

--- src/app/client/client-chart/client-chart.component.ts
+++ src/app/client/client-chart/client-chart.component.ts
@@ -1,215 +1,236 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Chart } from 'chart.js';
-import { Subscription } from 'rxjs/internal/Subscription';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
-import { Data } from '../client';
-import { Router } from '@angular/router';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-angular-link-http';
-import { HttpClient } from '@angular/common/http';
-import { WebSocketLink } from 'apollo-link-ws';
-import { setContext } from 'apollo-link-context';
-import { split } from 'apollo-link';
-import { getMainDefinition } from 'apollo-utilities';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Chart } from "chart.js";
+import { Subscription } from "rxjs/internal/Subscription";
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+import { Data, Client } from "../client";
+import { Router } from "@angular/router";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-angular-link-http";
+import { HttpClient } from "@angular/common/http";
+import { WebSocketLink } from "apollo-link-ws";
+import { setContext } from "apollo-link-context";
+import { split, Observable } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import { Title } from "@angular/platform-browser";
+import { ClientService } from "../client.service";
 // import io from 'socket.io-client';
 
 // const socket = io('http://localhost:3000');
 
 const subscription = gql`
-subscription pv {
-  pyranometer(limit:1, order_by:{input_time:desc}){
-    message
-    input_time
+  subscription pv {
+    pyranometer(limit: 1, order_by: { input_time: desc }) {
+      message
+      input_time
+    }
   }
-}
 `;
 
-const pvQuery = gql `query pv {
-  pv(limit: 10) {
-    topic
-    message
-    input_time
+const pvQuery = gql`
+  query pv {
+    pv(limit: 10) {
+      topic
+      message
+      input_time
+    }
   }
-}`
-const dconQuery = gql `query dcon {
-  dcon(limit:10)
-  {topic
-  message
-  input_time
+`;
+const dconQuery = gql`
+  query dcon {
+    dcon(limit: 10) {
+      topic
+      message
+      input_time
+    }
   }
-}`
-const inverterQuery = gql `query inverter {
-  inverter(limit:10)
-  {topic
-  message
-  input_time
+`;
+const inverterQuery = gql`
+  query inverter {
+    inverter(limit: 10) {
+      topic
+      message
+      input_time
+    }
   }
-}`
-const stateQuery = gql `query state {
-  state(limit:10)
-  {topic
-  message
-  input_time
+`;
+const stateQuery = gql`
+  query state {
+    state(limit: 10) {
+      topic
+      message
+      input_time
+    }
   }
-}`
-const pyranometerQuery = gql `query pyranometer {
-  pyranometer(limit:10)
-  {topic
-  message
-  input_time
+`;
+const pyranometerQuery = gql`
+  query pyranometer {
+    pyranometer(limit: 10) {
+      topic
+      message
+      input_time
+    }
   }
-}
-`
+`;
 
 @Component({
-  selector: 'app-client-chart',
-  templateUrl: './client-chart.component.html',
-  styleUrls: ['./client-chart.component.css']
+  selector: "app-client-chart",
+  templateUrl: "./client-chart.component.html",
+  styleUrls: ["./client-chart.component.css"],
 })
-export class ClientChartComponent implements OnInit,OnDestroy {
-
-
-
+export class ClientChartComponent implements OnInit, OnDestroy {
   todoSubscription: Subscription;
- 
 
-  titlePV 
-  dataPV 
-  titleDcon
-  dataDcon
-  titleInverter
-  dataInverter
-  titleState
-  dataState
-  titlePyranometer
-  dataPyranometer 
-  lastsegment
-  _uri ='http://35.173.73.235:8080/v1alpha1/graphql'
-  _uriWs ='ws://35.173.73.235:8080/v1alpha1/graphql'
+  titlePV;
+  dataPV;
+  titleDcon;
+  dataDcon;
+  titleInverter;
+  dataInverter;
+  titleState;
+  dataState;
+  titlePyranometer;
+  dataPyranometer;
+  lastsegment;
+  _uri = "http://35.173.73.235:8080/v1alpha1/graphql";
+  _uriWs = "ws://35.173.73.235:8080/v1alpha1/graphql";
+  client$: Client;
 
-
-  constructor(private apollo: Apollo,
+  constructor(
+    private apollo: Apollo,
     private router: Router,
-    private httpClient: HttpClient
-     ) {}
+    private httpClient: HttpClient,
+    private service: ClientService,
+    private titleService: Title
+  ) {}
 
-ngOnInit() {
-  var parts = this.router.url.split('/');
-  this.lastsegment= parts.pop() || parts.pop();  // handle potential trailing slash
+  ngOnInit() {
+    var parts = this.router.url.split("/");
+    this.lastsegment = parts.pop() || parts.pop(); // handle potential trailing slash
+    this.getClient(this.lastsegment);
 
-  
-{
-     const httpLink = new HttpLink(this.httpClient).create({
-      uri: this._uri,
-
-    });
-
-    const subscriptionLink = new WebSocketLink({
-      uri:this._uriWs,
-        
-        options: {
-            reconnect: true,
-            connectionParams: {
-            headers: {
-              "x-hasura-admin-secret": "mylongsecretkey"
-            }
-        }
-      }
-    });
-
-    const auth = setContext((operation, context) => ({
-      headers: {
-        "x-hasura-admin-secret": "mylongsecretkey"
-      },
-    }));
-
-    const link =split(
-      ({ query }) => {  
-        let definition  = getMainDefinition(query);
-        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
-      },
-      subscriptionLink,
-      auth.concat(httpLink),
-    );
-      this.apollo.create({
-      link,
-     cache: new InMemoryCache()
-    });
-}
-
-//PV
-    this.apollo.watchQuery({
-      query: pvQuery
-    }).valueChanges
-    .subscribe(({ data, loading }) => {
-      var key1 = Object.keys(data)
-      this.titlePV = Object.keys(data[key1.toString()][0])
-      this.titlePV.pop("__typename")
-      this.dataPV = data[key1.toString()]
-    })
-//DCON
-    this.apollo.watchQuery({
-      query: dconQuery
-    }).valueChanges
-    .subscribe(({ data, loading }) => {
-      var key1 = Object.keys(data)
-      this.titleDcon= Object.keys(data[key1.toString()][0])
-      this.titleDcon.pop("__typename")
-      this.dataDcon = data[key1.toString()]
-      
-    })
-
-
-//INVERTER
-    this.apollo.watchQuery({
-      query: inverterQuery
-    }).valueChanges
-    .subscribe(({ data, loading }) => {
-      var key1 = Object.keys(data)
-      this.titleInverter = Object.keys(data[key1.toString()][0])
-      this.titleInverter.pop("__typename")
-      this.dataInverter = data[key1.toString()]
-    })
-
-//State
-    this.apollo.watchQuery({
-      query: stateQuery
-    }).valueChanges
-    .subscribe(({ data, loading }) => {
-      var key1 = Object.keys(data)
-      this.titleState = Object.keys(data[key1.toString()][0])
-      this.titleState.pop("__typename")
-      this.dataState = data[key1.toString()]
-    })
-
-
-///Pyranometer
-    this.apollo.watchQuery({
-      query: pyranometerQuery
-    }).valueChanges
-    .subscribe(({ data, loading }) => {
-      var key1 = Object.keys(data)
-      this.titlePyranometer = Object.keys(data[key1.toString()][0])
-      this.titlePyranometer.pop("__typename")
-      this.dataPyranometer = data[key1.toString()]
-    })
-
+    {
+    }
   }
 
   ngOnDestroy() {
-    this.apollo.removeClient()
+    this.apollo.removeClient();
   }
-  getQueryResult(_query,_dataToPlace,_title){
-    this.apollo.watchQuery({
-      query: _query
-    }).valueChanges
-    .subscribe(({ data, loading }) => {
-    _dataToPlace = data
-     var key1 = Object.keys(data)
-    _title = Object.keys(data[key1.toString()][0])
-    
-   });
+  getQueryResult(_query, _dataToPlace, _title) {
+    this.apollo
+      .watchQuery({
+        query: _query,
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        _dataToPlace = data;
+        var key1 = Object.keys(data);
+        _title = Object.keys(data[key1.toString()][0]);
+      });
   }
 
+  getClient(id: any) {
+    this.service.getClient(id).subscribe(
+      (client) => {
+        this.client$ = client[0];
+        const httpLink = new HttpLink(this.httpClient).create({
+          uri: this.client$.urlHasura,
+        });
+
+        const subscriptionLink = new WebSocketLink({
+          uri: this._uriWs,
+
+          options: {
+            reconnect: true,
+            connectionParams: {
+              headers: {
+                "x-hasura-admin-secret": "mylongsecretkey",
+              },
+            },
+          },
+        });
+
+        const auth = setContext((operation, context) => ({
+          headers: {
+            "x-hasura-admin-secret": "mylongsecretkey",
+          },
+        }));
+
+        const link = split(
+          ({ query }) => {
+            let definition = getMainDefinition(query);
+            return (
+              definition.kind === "OperationDefinition" &&
+              definition.operation === "subscription"
+            );
+          },
+          subscriptionLink,
+          auth.concat(httpLink)
+        );
+        this.apollo.create({
+          link,
+          cache: new InMemoryCache(),
+        });
+
+        //PV
+        this.apollo
+          .watchQuery({
+            query: pvQuery,
+          })
+          .valueChanges.subscribe(({ data, loading,errors }) => {
+            var key1 = Object.keys(data);
+            this.titlePV = Object.keys(data[key1.toString()][0]);
+            this.titlePV.pop("__typename");
+            this.dataPV = data[key1.toString()];
+            console.log(loading)
+          });
+        //DCON
+        this.apollo
+          .watchQuery({
+            query: dconQuery,
+          })
+          .valueChanges.subscribe(({ data, loading }) => {
+            var key1 = Object.keys(data);
+            this.titleDcon = Object.keys(data[key1.toString()][0]);
+            this.titleDcon.pop("__typename");
+            this.dataDcon = data[key1.toString()];
+          });
+
+        //INVERTER
+        this.apollo
+          .watchQuery({
+            query: inverterQuery,
+          })
+          .valueChanges.subscribe(({ data, loading }) => {
+            var key1 = Object.keys(data);
+            this.titleInverter = Object.keys(data[key1.toString()][0]);
+            this.titleInverter.pop("__typename");
+            this.dataInverter = data[key1.toString()];
+          });
+
+        //State
+        this.apollo
+          .watchQuery({
+            query: stateQuery,
+          })
+          .valueChanges.subscribe(({ data, loading }) => {
+            var key1 = Object.keys(data);
+            this.titleState = Object.keys(data[key1.toString()][0]);
+            this.titleState.pop("__typename");
+            this.dataState = data[key1.toString()];
+          });
+        ///Pyranometer
+        this.apollo
+          .watchQuery({
+            query: pyranometerQuery,
+          })
+          .valueChanges.subscribe(({ data, loading }) => {
+            var key1 = Object.keys(data);
+            this.titlePyranometer = Object.keys(data[key1.toString()][0]);
+            this.titlePyranometer.pop("__typename");
+            this.dataPyranometer = data[key1.toString()];
+          });
+      },
+      (error) => console.log("HAI")
+    );
+  }
 }
--- src/app/client/client-detail/client-detail.component.ts
+++ src/app/client/client-detail/client-detail.component.ts
@@ -1,74 +1,67 @@
-
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { ClientService }  from '../client.service';
-import { Client } from '../client';
-import {Title} from "@angular/platform-browser";
-import { ToastrService } from 'ngx-toastr'
-
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { ClientService } from "../client.service";
+import { Client } from "../client";
+import { Title } from "@angular/platform-browser";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-client-detail',
-  templateUrl: './client-detail.component.html',
-  styleUrls: ['./client-detail.component.css']
+  selector: "app-client-detail",
+  templateUrl: "./client-detail.component.html",
+  styleUrls: ["./client-detail.component.css"],
 })
 export class ClientDetailComponent implements OnInit {
-  client$: Client
-  public url : string;
+  client$: Client;
+  public url: string;
 
   constructor(
     private router: Router,
     private service: ClientService,
-    private titleService:Title,
-    private toastr : ToastrService
+    private titleService: Title,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit() {
-    var parts = this.router.url.split('/');
-    var lastSegment = parts.pop() || parts.pop();  // handle potential trailing slash
-    this.getClient(lastSegment)
+    var parts = this.router.url.split("/");
+    var lastSegment = parts.pop() || parts.pop(); // handle potential trailing slash
+    this.getClient(lastSegment);
   }
 
-  onChange(UpdatedUrl : string) :void
-  {
+  onChange(UpdatedUrl: string): void {
     this.url = UpdatedUrl;
   }
 
-  getClient(id : any) {
-    
-    this.service.getClient(id)
-      .subscribe(
-        client => {
-          this.client$ = client[0];
-          console.log(this.client$.urlHasura)
-          this.titleService.setTitle(this.client$.name)
-          this.url = this.client$.url;
-        },
-        error => this.router.navigateByUrl('/login')
-        )
+  getClient(id: any) {
+    this.service.getClient(id).subscribe(
+      (client) => {
+        this.client$ = client[0];
+        console.log(this.client$.urlHasura);
+        this.titleService.setTitle(this.client$.name);
+        this.url = this.client$.url;
+      },
+      (error) => this.router.navigateByUrl("/login")
+    );
   }
 
-  save(client : Client) {
-    this.service.updateClient(client)
-      .subscribe(
-        client => {
+  save(client: Client) {
+    this.service.updateClient(client).subscribe(
+      (client) => {
         this.client$ = client;
         this.titleService.setTitle(client.name);
-        this.showSuccess("Client data updated Succesfully")
-        },
-    
-        error =>{
-          this.showError()
-        });
+        this.showSuccess("Client data updated Succesfully");
+      },
 
+      (error) => {
+        this.showError();
+      }
+    );
   }
 
-  showSuccess(message : string){
-    this.toastr.success(message, 'Success Info');
+  showSuccess(message: string) {
+    this.toastr.success(message, "Success Info");
   }
 
-  showError(){
-    this.toastr.error('Error!!', 'Error Info')
+  showError() {
+    this.toastr.error("Error!!", "Error Info");
   }
-
 }
